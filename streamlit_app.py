28f677a2d08deee371680ce71dd4a1a86f51e5b9
ao49it <63203355+ao49it@users.noreply.github.com>
2023-04-15 13:09:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,24 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+# Récupération des données
+url = "https://us-cert.cisa.gov/api/v1/summary"
+data = pd.read_json(url)
+data = pd.json_normalize(data["data"])
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Affichage de l'interface utilisateur
+st.title("Vulnérabilités les plus exploitées")
+product = st.selectbox("Produit", data.product.unique())
+chart_type = st.selectbox("Type de graphique", ["Barres", "Camembert"])
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Création du graphique
+filtered_data = data[data.product == product]
+counts = filtered_data.groupby("vulnerability_type").size()
+if chart_type == "Barres":
+    plt.bar(counts.index, counts.values)
+else:
+    plt.pie(counts.values, labels=counts.index)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Affichage du graphique
+st.pyplot()
